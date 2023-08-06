988a5a1cb9378a6122b497fe0c5544b35d2e4d13
Marco Silvestri <m.silvestri03@gmail.com>
2023-08-06 22:17:09 +0200

    add logo, rm set cursor, optimize size

--- config.h
+++ config.h
@@ -17,23 +17,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define MATRIX_ROWS 4
 #define MATRIX_COLS 4
-
 #define MATRIX_ROW_PINS { F7, F6, F5, F4 }
 #define MATRIX_COL_PINS { B6, B2, B3, B1 }
-
 #define DIODE_DIRECTION COL2ROW
-
 #define DEBOUNCE 5
-
 #define AUDIO_PIN B5
-
 #define OLED_DISPLAY_128X64
-
 #define NUMBER_OF_ENCODERS 2
 #define ENCODERS_PAD_A { B4, C6 }
 #define ENCODERS_PAD_B { E6, D4 }
 #define ENCODER_RESOLUTION 4
-
 #define DIP_SWITCH_PINS { D7, D3 }
 #define AUDIO_CLICKY
 #define AUDIO_CLICKY_FREQ_RANDOMNESS 0.02f
+#define NO_MUSIC_MODE
