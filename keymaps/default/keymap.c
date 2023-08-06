0cd0fd9ccd345dd89a535845ae637ebebef26821
Marco Silvestri <m.silvestri03@gmail.com>
2023-08-06 12:07:48 +0200

    fixed switch to handle custom keys

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -503,7 +503,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             };
             break;
         default:
-            return false;
+            break;
     }
     return true;
 }
